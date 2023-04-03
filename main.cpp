122f07d9b75c9f4d7acd1924afc426e64c784b3c
clem <mail@clemens-cords.com>
2023-04-03 21:00:14 +0200

    stashing g attachment

--- main.cpp
+++ main.cpp
@@ -5,12 +5,44 @@
 
 using namespace mousetrap;
 
+#include <include/g_object_attachment.hpp>
+
+using VoidFunction = std::function<void(Button*)>;
+#define G_TYPE_VOID_FUNCTION_WRAPPER (void_function_wrapper_get_type())
+G_NEW_WRAPPER(VoidFunction, void_function, VOID_FUNCTION)
+
+struct ButtonCallback : public Widget
+{
+    Box _box = Box(Orientation::HORIZONTAL);
+    Button _button;
+
+    std::function<void(Button*)>* _callback = nullptr;
+
+    ButtonCallback()
+    {
+        _box.push_back(&_button);
+    }
+
+    operator NativeWidget() const override
+    {
+        return _box;
+    }
+
+    template<typename Function_t>
+    void register_callback(Function_t f)
+    {
+        _callback = new std::function<void(Button*)>(f);
+        _button.connect_signal_clicked([](Button* button, std::function<void(Button*)>* f){
+            (*f)(button);
+        }, _callback);
+
+        detail::attach_ref_to_object(G_OBJECT(_button.operator GtkWidget *()), wrap(_callback));
+    }
+};
+
 inline struct State {
     Window window;
-    Button button;
 }* state = nullptr;
-
-#include <include/g_object_attachment.hpp>
 
 int main()
 {
@@ -22,11 +54,12 @@ int main()
     Window(*app)
         };
 
-        using namespace detail;
-        static auto* test_item = detail::wrapper_new(new detail::Test(13));
-        detail::attach_ref_to_object(state->window.operator GObject*(), G_OBJECT(test_item));
+        auto callback = ButtonCallback();
+        callback.register_callback([](Button* button){
+            std::cout << "success" << std::endl;
+        });
 
-        state->window.set_child(&state->button);
+        state->window.set_child(&callback);
         state->window.present();
     });
 
--- mousetrap/include/g_object_attachment.hpp
+++ mousetrap/include/g_object_attachment.hpp
@@ -25,67 +25,93 @@ namespace mousetrap::detail
 
     // WRAPPER IMPLEMENTATION
 
-    #define G_TYPE_WRAPPER (wrapper_get_type())
+    /// @brief generate a new gobject wrapper
+    /// @param T CamelCase identifier of the value to be wrapped, this has to be an already valid class name
+    /// @param t snake_case identifier of the value to be wrapped
+    /// @param T_CAPS CAPS_CASE identifier of the vaue to be wrapped
+    /// @example
+    /// \code
+    ///     #define G_TYPE_TEST_WRAPPER (test_wrapper_get_type())
+    ///     G_NEW_WRAPPER(Test, test, TEST)
+    /// \endcode
+    #define G_NEW_WRAPPER(T, t, T_CAPS) \
+        G_DECLARE_FINAL_TYPE(T##Wrapper, t##_wrapper, G, T_CAPS##_WRAPPER, GObject) \
+                                        \
+        \
+        struct _##T##Wrapper \
+        { \
+            GObject parent_instance; \
+            T* data; \
+        }; \
+        \
+        struct _##T##Wrapper##Class \
+        { \
+            GObjectClass parent_class; \
+        }; \
+        \
+        G_DEFINE_TYPE (T##Wrapper, t##_wrapper, G_TYPE_OBJECT) \
+        \
+        static void t##_wrapper_finalize (GObject *object) \
+        { \
+            auto* self = G_##T_CAPS##_WRAPPER(object); \
+            printf("destroy %p", self->data);\
+            delete self->data;          \
+            G_OBJECT_CLASS(t##_wrapper_parent_class)->finalize(object); \
+        } \
+        \
+        static void t##_wrapper_init(T##Wrapper * item) \
+        { \
+            item->data = nullptr; \
+        } \
+        \
+        static void t##_wrapper_class_init(T##Wrapper##Class* c) \
+        { \
+            GObjectClass *gobject_class = G_OBJECT_CLASS (c); \
+             gobject_class->finalize = t##_wrapper_finalize; \
+        } \
+        \
+        static T##Wrapper* t##_wrapper_new(T* in) \
+        { \
+            auto* item = (T##Wrapper*) g_object_new(G_TYPE_##T_CAPS##_WRAPPER, nullptr); \
+            t##_wrapper_init(item); \
+            item->data = in; \
+            return item; \
+        }                               \
+                                        \
+        static GObject* wrap(T* in)         \
+        {       \
+            return G_OBJECT(t##_wrapper_new(in));                                \
+        }                                \
 
-    G_DECLARE_FINAL_TYPE(Wrapper, wrapper, G, WRAPPER, GObject)
-
-    struct _Wrapper
+    namespace internal
     {
-        GObject parent_instance;
-        Test* data;
-    };
-
-    struct _WrapperClass
-    {
-        GObjectClass parent_class;
-    };
-
-    G_DEFINE_TYPE (Wrapper, wrapper, G_TYPE_OBJECT)
-
-    static void wrapper_finalize (GObject *object)
-    {
-        auto* self = G_WRAPPER(object);
-        delete self->data;
-        G_OBJECT_CLASS(wrapper_parent_class)->finalize(object);
-    }
-
-    static void wrapper_init(Wrapper* item)
-    {
-        item->data = nullptr;
-    }
-
-    static void wrapper_class_init(WrapperClass* c)
-    {
-        GObjectClass *gobject_class = G_OBJECT_CLASS (c);
-        gobject_class->finalize = wrapper_finalize;
-    }
-
-    static Wrapper* wrapper_new(Test* in)
-    {
-        auto* item = (Wrapper*) g_object_new(G_TYPE_WRAPPER, nullptr);
-        wrapper_init(item);
-        item->data = in;
-        return item;
-    }
-
-    /// @brief function that releases attached gobject ref when objects reference count reaches 0
-    /// @param data attached object
-    /// @param object parent object
-    /// @param gboolean true if triggered while parent has exactly 1 reference left
-    static void toggle_notify(gpointer data, GObject* object, gboolean last_ref)
-    {
-        if (last_ref)
+        /// @brief function that releases attached gobject ref when objects reference count reaches 0
+        /// @param attachment attached object
+        /// @param object parent object
+        /// @param gboolean true if triggered while parent has exactly 1 reference left
+        static void toggle_notify(gpointer attachment, GObject* object, gboolean last_ref)
         {
-            g_object_unref(data);
-            g_object_remove_toggle_ref(object, toggle_notify, data);
+            if (last_ref and G_IS_OBJECT(attachment))
+            {
+                g_object_unref(attachment);
+                g_object_remove_toggle_ref(object, toggle_notify, attachment);
+            }
         }
     }
 
     /// @brief attach a gobject wrapper to an arbitrary GObject parent. If parents reference count reaches 0, the attachment is freed.
     /// @param parent parent object, if this object goes out of scope, attachment is deleted too
-    /// @param attachment if this object goes out of scope, parent is unaffected
+    /// @param attachment object to be deleted when parent goes out of scope
     static void attach_ref_to_object(GObject* parent, GObject* attachment)
     {
-        g_object_add_toggle_ref(parent, toggle_notify, attachment);
+        g_object_add_toggle_ref(parent, internal::toggle_notify, attachment);
+    }
+
+    /// @brief detach a gobject wrapper from an arbitrary GObject parent
+    /// @param parent parent object, if this object goes out of scope, attachment is deleted too
+    /// @param attachment object to be deleted when parent goes out of scope
+    static void detach_ref_from_object(GObject* parent, GObject* attachment)
+    {
+        g_object_remove_toggle_ref(parent, internal::toggle_notify, attachment);
     }
 }
