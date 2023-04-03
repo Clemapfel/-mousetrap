f821469a879ad0e91641cb82d8959cd9f9ad4331
clem <mail@clemens-cords.com>
2023-04-03 17:09:01 +0200

    stashing before widget inheritance rework

--- main.cpp
+++ main.cpp
@@ -5,9 +5,25 @@
 
 using namespace mousetrap;
 
+class CompoundWidget : public Widget
+{
+    private:
+        Box _box = Box(Orientation::HORIZONTAL);
+        Button _button_01, _button_02, _button_03;
+
+    public:
+        CompoundWidget()
+            : Widget(_box.operator GtkWidget*())
+        {
+            _button_02.set_child(&_button_03);
+            _button_01.set_child(&_button_02);
+            _box.push_back(&_button_01);
+        }
+};
+
 inline struct State {
     Window window;
-
+    CompoundWidget widget;
 }* state = nullptr;
 
 int main()
@@ -17,9 +33,11 @@ int main()
     app.connect_signal_activate([](Application* app) -> void
     {
         state = new State {
-        Window(*app)
+    Window(*app)
         };
 
+        state->window.set_child(&state->widget);
+        //state->window.set_child(&state->window);
         state->window.present();
     });
 
