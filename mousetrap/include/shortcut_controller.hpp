f1afe0ae8f1482a2987e22a3c6867de66ddc5286
clem <mail@clemens-cords.com>
2023-04-08 21:38:11 +0200

    added shortcut controller

---
+++ mousetrap/include/shortcut_controller.hpp
@@ -0,0 +1,58 @@
+//
+// Copyright 2022 Clemens Cords
+// Created on 10/22/22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <include/event_controller.hpp>
+#include <include/application.hpp>
+
+#include <unordered_map>
+
+namespace mousetrap
+{
+    /// @brief declares scope in which a shortcut controller should capture shortcut events
+    enum ShortcutScope
+    {
+        /// @brief captured by widget the controller belongs to
+        LOCAL = GTK_SHORTCUT_SCOPE_LOCAL,
+        /// @brief captured by first ancestor that is a PopoverMenu or Window
+        MANAGED = GTK_SHORTCUT_SCOPE_MANAGED,
+        /// @brief always captured
+        GLOBAL = GTK_SHORTCUT_SCOPE_GLOBAL
+    };
+
+    /// @brief event controller that manages shortcuts, you do not need to connect to any signals of this class
+    class ShortcutController : public EventController
+    {
+        public:
+            /// @brief construct
+            /// @param app Application actions are registered to
+            ShortcutController(Application*);
+
+            /// @brief destruct
+            ~ShortcutController();
+
+            /// @brief add action, if the action has a shortcut and it is registered with the application, shortcut controller will activate the action when the shortcut is pressed
+            /// @param action
+            void add_action(const Action& id);
+
+            /// @brief remove an action, shortcut controller will no longer activate it
+            /// @param action_id
+            void remove_action(ActionID id);
+
+            /// @brief set the controllers scope, ShortcutScope::GLOBAL by default
+            /// @param scope
+            void set_scope(ShortcutScope);
+
+            /// @brief get the controllers scope
+            /// @return scope
+            ShortcutScope get_scope();
+
+        private:
+            Application* _action_map;
+            std::unordered_map<ActionID, std::vector<GtkShortcut*>> _shortcuts;
+    };
+}
+
